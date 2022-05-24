ebbf7eb46fff2a874e8218d0ffa7e117f465fcdd
stasofob <64842680+stasofob@users.noreply.github.com>
2022-05-24 17:23:02 +0300

    добавил прототип модели, но не проверял

--- main/models.py
+++ main/models.py
@@ -1,1 +1,75 @@
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 from django.db import models
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Email непременно должен быть указан')
+
+        user = self.model(
+            email=UserManager.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(
+        'Электронная почта',
+        max_length=255,
+        unique=True,
+        db_index=True
+    )
+    firstname = models.CharField(
+        'Имя',
+        max_length=40,
+    )
+    lastname = models.CharField(
+        'Фамилия',
+        max_length=40,
+    )
+    middlename = models.CharField(
+        'Отчество',
+        max_length=40,
+        null=True,
+        blank=True
+    )
+    is_admin = models.BooleanField(
+        'Суперпользователь',
+        default=False
+    )
+
+    def get_full_name(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+    def get_short_name(self):
+        return self.email
+
+    def __str__(self):
+        return self.lastname + ' ' + self.firstname + ' ' + self.middlename
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
