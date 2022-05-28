fd1dbde50f8c9e446a9d3c56a47de6264bd5dd30
stasofob <andreeff.stas2014@gmail.com>
2022-05-28 14:37:15 +0300

    отредактировал представление AdminModel

--- main/admin.py
+++ main/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import MyUser
 from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(MyUser, UserAdmin)
+
+class MyUserAdmin(admin.ModelAdmin):
+    fields = ('last_name', 'first_name', 'middle_name',
+              'email', 'is_active')
+
+
+admin.site.register(MyUser, MyUserAdmin)
