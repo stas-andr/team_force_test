5f1d1426a1ed25691349a947f42424147ac1178e
Stas Andreev <64842680+stasofob@users.noreply.github.com>
2022-06-02 16:57:48 +0300

    добавил создание профиля при создании юзера

--- main/admin.py
+++ main/admin.py
@@ -3,12 +3,6 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
 from .models import MyUser, Profile, Tag
 
-
-class MyUserAdmin(UserAdmin):
-    list_display = ('last_name', 'first_name', 'middle_name',
-                    'username', 'email')
-
-
-admin.site.register(MyUser, MyUserAdmin)
+admin.site.register(MyUser, UserAdmin)
 admin.site.register(Profile)
 admin.site.register(Tag)
--- main/models.py
+++ main/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class MyUser(AbstractUser):
     middle_name = models.CharField("Отчество", max_length=150, blank=True)
 
     def __str__(self):
-        return f'{self.last_name} {self.first_name} {self.middle_name}'
+        return f'{self.username}: {self.last_name} {self.first_name} {self.middle_name}'
 
 
 class Tag(models.Model):
@@ -31,6 +33,11 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.__str__()}'
 
+
+@receiver(post_save, sender=MyUser)
+def create_profile(sender, instance, **kwargs):
+    if kwargs['created']:
+        Profile.objects.create(user=instance)
 
 
 
